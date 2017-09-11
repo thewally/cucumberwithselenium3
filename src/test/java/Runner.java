09385e7e3ee74cb1d5441377ef2a576c03255360
arjen <arjen@detesters.nl>
2017-09-11 12:19:09 +0200

    Initial commit

---
+++ src/test/java/Runner.java
@@ -0,0 +1,15 @@
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        monochrome = true,
+        plugin = {"pretty", "html:target/cucumber"},
+        dryRun = false,
+        glue = {"nl.thewally.stepdefs"},
+        features = {"src/test/resources/features"}
+//        ,tags = {"@x"}
+)
+public class Runner {
+}
