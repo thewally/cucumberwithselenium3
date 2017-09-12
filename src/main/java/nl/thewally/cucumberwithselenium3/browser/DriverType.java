6fd1f8588c1a2d97f314e2e04ccfb9c4e7908309
arjen <arjen@detesters.nl>
2017-09-12 16:11:40 +0200

    Removed JBrowser because not supported for Selenium 3

--- src/main/java/nl/thewally/cucumberwithselenium3/browser/DriverType.java
+++ src/main/java/nl/thewally/cucumberwithselenium3/browser/DriverType.java
@@ -2,15 +2,13 @@ package nl.thewally.cucumberwithselenium3.browser;
 
 import nl.thewally.cucumberwithselenium3.browser.types.Driver;
 import nl.thewally.cucumberwithselenium3.browser.types.Firefox;
-import nl.thewally.cucumberwithselenium3.browser.types.JBrowser;
 import nl.thewally.cucumberwithselenium3.browser.types.Phantomjs;
 
 public enum DriverType {
 
     FIREFOX(Firefox.class),
-    PHANTOMJS(Phantomjs.class),
+    PHANTOMJS(Phantomjs.class);
 //    CHROME(Chrome.class),
-    JBROWSER(JBrowser.class);
 
     private final Class<? extends Driver> driver;
 
