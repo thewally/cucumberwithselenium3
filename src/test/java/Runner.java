4a61ccbf1e781eabfa1bbe8e4abc37887806b97c
arjen <arjen@detesters.nl>
2017-11-23 13:54:22 +0100

    Fixed issue with HTML setHeaders

--- src/test/java/Runner.java
+++ src/test/java/Runner.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         monochrome = true,
-        plugin = {"pretty", "json:target/cucumber"},
+        plugin = {"pretty", "html:target/cucumber", "json:target/cucumber.json"},
         dryRun = false,
         glue = {"nl.thewally.cucumberwithselenium3.stepdefs"},
         features = {"src/test/resources/features"}
